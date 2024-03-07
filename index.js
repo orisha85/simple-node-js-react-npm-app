ae408d18003296a974c16aa81b25b4e363ca2dc9
isaac-solutions <93618046+isaac-solutions@users.noreply.github.com>
2024-03-07 15:55:20 -0500

    changed start script in package.json

--- index.js
+++
@@ -1,5 +0,0 @@
-var express = require('express'); 
-var server = express(); 
-var options = { index: 'public/index.html' };  // Adjusted path to public/index.html
-server.use('/', express.static('/home/site/wwwroot', options)); 
-server.listen(process.env.PORT);
