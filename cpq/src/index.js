e0d8ccaf245fdeea2ce72073fef9b3506f2b2c35
isaac-solutions <93618046+isaac-solutions@users.noreply.github.com>
2024-03-12 12:30:28 -0400

    disable next line for eslint error

--- cpq/src/index.js
+++ cpq/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+eslint-disable-next-line
 import Sidebar from './Modules/Sidebar'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
