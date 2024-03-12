e690b3a7186ca80c3bca4861886af131b7a24b28
isaac-solutions <93618046+isaac-solutions@users.noreply.github.com>
2024-03-12 09:53:19 -0400

    revert file structure changes and pivot off react-start functionality

---
+++ cpq/src/data/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    user: 'smc_readwrite',
+    password: 'Mxyk3x2023aplyi',
+    server: 'SMC-SQL-AZ',    
+    //server: '4.tcp.ngrok.io,12122',
+    //port: 0,
+    database: 'SMC_APP_DEV',
+    options: {
+        encrypt: true, // For Azure SQL
+        trustServerCertificate: true, // Change this depending on your certificate configuration
+        connectionTimeout: 15000,
+        enableArithAbort: true
+    },
+    pool: {
+        max: 20,
+        min: 0,
+        idleTimeoutMillis: 30000
+    }
+}
--- server/config_url.js
+++
@@ -1,5 +0,0 @@
-const port = 3008;
-module.exports = {
-    port: `${port}`,
-    url: `http://localhost:${port}`,
-}
