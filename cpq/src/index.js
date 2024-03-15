6f7a5a6d729c4630e45808b010527718f7d78d65
isaac-solutions <93618046+isaac-solutions@users.noreply.github.com>
2024-03-15 09:31:35 -0400

    moved web config down a folder and added build folder movement

--- cpq/src/index.js
+++ cpq/src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals.js';
 //eslint-disable-next-line
 //import Sidebar from './Modules/Sidebar'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
- 
   <div className="App">
     <App />
   </div>
-
-   
 );
 
 // If you want to start measuring performance in your app, pass a function
